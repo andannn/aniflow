a40a3e98165db2815db3472517a6adbb92964bab
andannn <jqn296763005@gmail.com>
2023-09-03 23:07:11 +0800

    initial git

---
+++ android/app/src/main/kotlin/com/andannn/animetracker/anime_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.andannn.animetracker.anime_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/anime_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anime_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
