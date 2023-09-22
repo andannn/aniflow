3442de6d225da296b69aac9809c70fe4750be197
andannn <jqn296763005@gmail.com>
2023-09-22 18:37:08 +0800

    repository

---
+++ lib/core/data/model/anime_list_item_model.dart
@@ -0,0 +1,27 @@
+import 'package:anime_tracker/core/data/model/short_anime_model.dart';
+import 'package:anime_tracker/core/data/repository/user_anime_list_repository.dart';
+import 'package:anime_tracker/core/database/model/relations/user_anime_list_and_anime.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'anime_list_item_model.freezed.dart';
+
+@freezed
+class AnimeListItemModel with _$AnimeListItemModel {
+  factory AnimeListItemModel({
+    @Default('') String id,
+    AnimeListStatus? status,
+    int? score,
+    int? updatedAt,
+    ShortAnimeModel? animeModel,
+  }) = _AnimeListItemModel;
+
+  static AnimeListItemModel fromDataBaseModel(UserAnimeListAndAnime model) {
+    return AnimeListItemModel(
+      id: model.userAnimeListEntity.id,
+      status: model.userAnimeListEntity.status,
+      score: model.userAnimeListEntity.score,
+      updatedAt: model.userAnimeListEntity.updatedAt,
+      animeModel: ShortAnimeModel.fromDatabaseModel(model.animeEntity),
+    );
+  }
+}
--- lib/core/data/repository/user_anime_list_repository.dart
+++ lib/core/data/repository/user_anime_list_repository.dart
@@ -1,11 +1,32 @@
-import 'package:anime_tracker/core/data/model/short_anime_model.dart';
-import 'package:anime_tracker/core/data/repository/ani_list_repository.dart';
+import 'package:anime_tracker/core/common/global_static_constants.dart';
+import 'package:anime_tracker/core/data/model/anime_list_item_model.dart';
+import 'package:anime_tracker/core/database/anime_database.dart';
+import 'package:anime_tracker/core/database/user_anime_list_dao.dart';
+import 'package:anime_tracker/core/network/ani_list_data_source.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part '../model/anime_list_status.dart';
 
 abstract class UserAnimeListRepository {
-  /// refresh the category anime table, which will deleted the table and get
-  /// data from network data source.
-  Future<LoadResult<ShortAnimeModel>> refreshAnimeByCategory();
+  Future<List<AnimeListItemModel>> getUserAnimeList(
+      {required AnimeListStatus status,
+      required int page,
+      int? userId,
+      int perPage = Config.defaultPerPageCount});
+}
+
+class UserAnimeListRepositoryImpl extends UserAnimeListRepository {
+  final UserAnimeListDao userAnimeListDao =
+      AnimeDatabase().getUserAnimeListDao();
+  final AniListDataSource dataSource = AniListDataSource();
+
+  @override
+  Future<List<AnimeListItemModel>> getUserAnimeList(
+      {required AnimeListStatus status,
+      required int page,
+      int? userId,
+      int perPage = Config.defaultPerPageCount}) {
+    
+    throw UnimplementedError();
+  }
 }
