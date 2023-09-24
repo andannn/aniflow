758d81d0c38c2fcf382040a14531e9bd6f94d7c9
andannn <jqn296763005@gmail.com>
2023-09-24 16:19:22 +0800

    bookmark label

--- lib/core/design_system/widget/anime_preview_item.dart
+++ lib/core/design_system/widget/anime_preview_item.dart
@@ -8,51 +8,74 @@ class AnimePreviewItem extends StatelessWidget {
   const AnimePreviewItem(
       {required this.model,
       required this.onClick,
+      this.isTracking = false,
       super.key,
       this.width,
       this.textStyle});
 
   final AnimeModel model;
+  final bool isTracking;
   final VoidCallback onClick;
   final double? width;
   final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      color: Theme.of(context).colorScheme.surfaceVariant,
-      clipBehavior: Clip.antiAlias,
-      child: InkWell(
-        onTap: onClick,
-        child: SizedBox(
-          width: width,
-          child: Column(children: [
-            AspectRatio(
-              aspectRatio: 3.0 / 4,
-              child: AFNetworkImage(
-                imageUrl: model.coverImage,
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  child: Text(
-                    model.title!.getLocalTitle(context),
-                    textAlign: TextAlign.center,
-                    style: textStyle?.copyWith(
-                        color: Theme.of(context).colorScheme.onSurfaceVariant),
-                    maxLines: 2,
-                    softWrap: true,
+    return Stack(
+      children: [
+        Card(
+          elevation: 0,
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: onClick,
+            child: SizedBox(
+              width: width,
+              child: Column(children: [
+                AspectRatio(
+                  aspectRatio: 3.0 / 4,
+                  child: AFNetworkImage(
+                    imageUrl: model.coverImage,
                   ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: Text(
+                        model.title!.getLocalTitle(context),
+                        textAlign: TextAlign.center,
+                        style: textStyle?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant),
+                        maxLines: 2,
+                        softWrap: true,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
             ),
-          ]),
+          ),
         ),
-      ),
+        isTracking
+            ? Align(
+                alignment: Alignment.topRight,
+                child: Transform.translate(
+                  offset: const Offset(-10, -3),
+                  child: Transform.scale(
+                    scaleY: 1.3,
+                    scaleX: 0.8,
+                    child: Icon(
+                      Icons.bookmark,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              )
+            : const SizedBox(),
+      ],
     );
   }
 }
