753ae55b383c61a85f0c6209a1a4e1aa3e800e03
andannn <jqn296763005@gmail.com>
2023-10-18 21:42:58 +0900

    fix tests

--- test/feature/anime_search/bloc/anime_search_bloc_test.dart
+++ test/feature/anime_search/bloc/anime_search_bloc_test.dart
@@ -1,12 +1,19 @@
 import 'package:anime_tracker/core/data/search_repository.dart';
+import 'package:anime_tracker/core/database/anime_database.dart';
 import 'package:anime_tracker/feature/anime_search/bloc/anime_search_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
   group('anime_search_bloc_test', () {
     late SearchPageBloc searchPageBloc;
+    final animeDatabase = AnimeDatabase();
 
     setUp(() async {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+
+      await animeDatabase.initDatabase(isTest: true);
       searchPageBloc = SearchPageBloc(searchRepository: SearchRepositoryImpl());
     });
 
