a623b0e2a57a4e83304b41fcb57f127118832dee
andannn <jqn296763005@gmail.com>
2023-12-07 17:11:17 +0800

    Set userData size and cache data size userProperty when launch

--- lib/core/firebase/firebase_analytics_util.dart
+++ lib/core/firebase/firebase_analytics_util.dart
@@ -1,6 +1,7 @@
 import 'package:aniflow/app/aniflow_router/ani_flow_route_path.dart';
 import 'package:aniflow/core/common/model/media_type.dart';
 import 'package:aniflow/core/common/util/logger.dart';
+import 'package:aniflow/core/firebase/data_size_property/data_type.dart';
 import 'package:aniflow/core/firebase/firebase_event.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
@@ -13,6 +14,19 @@ extension FirebaseAnalyticsExtension on FirebaseAnalytics {
         'screen_name': path.toString(),
       },
     );
+  }
+
+  Future computeAndSetAppDataSizeProperty() async {
+    for (var type in DataType.values) {
+      final size =
+          await ComputeFileSizeHelper.computeSizeOfDataTypeInIsolate(type);
+
+      final value = (size / 1000000).round();
+      await setUserProperty(
+        name: FirebaseUserProperty.fromDataType(type).propertyName,
+        value: '${value}_MB',
+      );
+    }
   }
 
   Future setUserMediaContentProperty(MediaType type) async {
--- lib/core/firebase/firebase_event.dart
+++ lib/core/firebase/firebase_event.dart
@@ -1,4 +1,6 @@
 
+import 'package:aniflow/core/firebase/data_size_property/data_type.dart';
+
 enum FirebaseEvent {
   screenTransaction(eventName: 'screen_transaction'),
   like(eventName: 'like'),
@@ -10,9 +12,16 @@ enum FirebaseEvent {
 }
 
 enum FirebaseUserProperty {
-  mediaContent(propertyName: 'media_content');
+  mediaContent(propertyName: 'media_content'),
+  imgCacheSize(propertyName: 'img_cache_size'),
+  userDataSize(propertyName: 'user_data_size');
 
   final String propertyName;
 
   const FirebaseUserProperty({required this.propertyName});
+
+  static FirebaseUserProperty fromDataType(DataType type) => switch (type) {
+    DataType.userData => FirebaseUserProperty.userDataSize,
+    DataType.imgCache => FirebaseUserProperty.imgCacheSize,
+  };
 }
