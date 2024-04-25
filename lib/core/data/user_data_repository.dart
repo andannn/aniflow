38609a15d0ba3b692a443d6b6174a6284822cb17
andannn <jqn296763005@gmail.com>
2024-04-25 10:13:54 +0900

    Some change

--- lib/core/data/user_data_repository.dart
+++ lib/core/data/user_data_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:aniflow/core/common/definitions/activity_filter_type.dart';
 import 'package:aniflow/core/common/definitions/activity_scope_category.dart';
 import 'package:aniflow/core/common/definitions/ani_list_settings.dart';
@@ -5,62 +7,66 @@ import 'package:aniflow/core/common/definitions/anime_season.dart';
 import 'package:aniflow/core/common/definitions/media_type.dart';
 import 'package:aniflow/core/common/setting/theme_setting.dart';
 import 'package:aniflow/core/data/model/user_data_model.dart';
+import 'package:aniflow/core/firebase/firebase_analytics_util.dart';
 import 'package:aniflow/core/shared_preference/user_data_preferences.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
-class UserDataRepository implements UserDataPreferences {
+class UserDataRepository {
   UserDataRepository(this._preferences);
 
   final UserDataPreferences _preferences;
 
-  @override
   Stream<UserDataModel> get userDataStream => _preferences.userDataStream;
 
-  @override
   UserDataModel get userData => _preferences.userData;
 
-  @override
   Future setActivityFilterType(ActivityFilterType category) =>
       _preferences.setActivityFilterType(category);
 
-  @override
   Future setActivityScopeCategory(ActivityScopeCategory scopeCategory) =>
       _preferences.setActivityScopeCategory(scopeCategory);
 
-  @override
   Future setAniListSettings(AniListSettings setting) =>
       _preferences.setAniListSettings(setting);
 
-  @override
   Future setAnimeSeason(AnimeSeason season) =>
       _preferences.setAnimeSeason(season);
 
-  @override
   Future setAuthExpiredTime(DateTime? dateTime) =>
       _preferences.setAuthExpiredTime(dateTime);
 
-  @override
   Future setAuthToken(String? authToken) =>
       _preferences.setAuthToken(authToken);
 
-  @override
   Future setAuthedUserId(String? userId) =>
       _preferences.setAuthedUserId(userId);
 
-  @override
   Future setIsShowReleaseOnly(bool showReleasedOnly) =>
       _preferences.setIsShowReleaseOnly(showReleasedOnly);
 
-  @override
-  Future setMediaType(MediaType mediaType) =>
-      _preferences.setMediaType(mediaType);
+  Future setMediaType(MediaType mediaType) async {
+    await _preferences.setMediaType(mediaType);
+    unawaited(
+        FirebaseAnalytics.instance.setUserMediaContentProperty(mediaType));
+  }
 
-  @override
   Future setSeasonYear(int seasonYear) =>
       _preferences.setSeasonYear(seasonYear);
 
-  @override
   Future setThemeSetting(ThemeSetting setting) =>
       _preferences.setThemeSetting(setting);
+
+  AnimeSeasonParam getAnimeSeasonParam() {
+    final seasonYear = _preferences.userData.seasonYear;
+    final season = _preferences.userData.season;
+
+    return AnimeSeasonParam(seasonYear: seasonYear, season: season);
+  }
+
+  Future setAnimeSeasonParam(AnimeSeasonParam param) async {
+    await _preferences.setAnimeSeason(param.season);
+    await _preferences.setSeasonYear(param.seasonYear);
+  }
 }
