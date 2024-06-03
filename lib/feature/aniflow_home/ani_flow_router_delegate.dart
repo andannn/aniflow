69198111365581a51dcfe8c9eee6faa4697b5f42
andannn <jqn296763005@gmail.com>
2024-06-03 17:15:06 +0900

    test

--- lib/feature/aniflow_home/ani_flow_router_delegate.dart
+++ lib/feature/aniflow_home/ani_flow_router_delegate.dart
@@ -1,33 +1,43 @@
 import 'package:aniflow/feature/aniflow_home/top_level_navigation.dart';
 import 'package:flutter/material.dart';
 
-class AfRouterDelegate extends RouterDelegate<TopLevelRoutePath>
+class AfRouterDelegate extends RouterDelegate<TopLevelNavigation>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin {
   AfRouterDelegate();
 
   final GlobalKey<NavigatorState> _navigatorKey = GlobalKey();
+  final GlobalKey<_TopLevelNavigatorState> _topLevelStateKey = GlobalKey();
 
   @override
   GlobalKey<NavigatorState>? get navigatorKey => _navigatorKey;
 
-
-  List<TopLevelRoutePath> _backStack = [const DiscoverRoutePath()];
-
   /// get current top level.
-  TopLevelNavigation get currentTopLevelNavigation =>
-      _backStack.last.topLevel;
+  RestorableEnum<TopLevelNavigation> get _topLevelState =>
+      _topLevelStateKey.currentState!.topLevel;
+
+  TopLevelNavigation get currentTopLevelNavigation => _topLevelState.value;
 
   @override
   Widget build(BuildContext context) {
-    if (_backStack.isEmpty) {
-      return const SizedBox();
-    }
-
-    return Navigator(
-      key: navigatorKey,
-      pages: _backStack.map((path) => path.generatePage()).toList(),
+    return TopLevelNavigator(
+      key: _topLevelStateKey,
+      navigatorKey: _navigatorKey,
       onPopPage: _onPopPage,
     );
+  }
+
+  @override
+  Future<void> setNewRoutePath(TopLevelNavigation configuration) {
+    throw UnimplementedError();
+  }
+
+  void navigateToTopLevelPage(TopLevelNavigation navigation) {
+    _topLevelState.value = navigation;
+    notifyListeners();
+
+// TODO:
+//     FirebaseAnalytics.instance
+//         .logAniFlowPathChangeEvent(navigation.toRoutePath());
   }
 
   /// Judgment of whether need to pop current page.
@@ -36,27 +46,55 @@ class AfRouterDelegate extends RouterDelegate<TopLevelRoutePath>
       return false;
     }
 
-    _backStack.removeLast();
+    _topLevelState.value = TopLevelNavigation.discover;
     notifyListeners();
+
     return true;
+  }
+}
+
+class TopLevelNavigator extends StatefulWidget {
+  const TopLevelNavigator({
+    super.key,
+    required this.navigatorKey,
+    this.onPopPage,
+  });
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final PopPageCallback? onPopPage;
+
+  @override
+  State<TopLevelNavigator> createState() => _TopLevelNavigatorState();
+}
+
+class _TopLevelNavigatorState extends State<TopLevelNavigator>
+    with RestorationMixin {
+  final topLevel = RestorableEnum<TopLevelNavigation>(
+    TopLevelNavigation.discover,
+    values: TopLevelNavigation.values,
+  );
+
+  Set<TopLevelNavigation> get _backStack =>
+      {TopLevelNavigation.discover, topLevel.value};
+
+  @override
+  Widget build(BuildContext context) {
+    if (_backStack.isEmpty) {
+      return const SizedBox();
+    }
+
+    return Navigator(
+      key: widget.navigatorKey,
+      pages: _backStack.map((path) => path.toPage()).toList(),
+      onPopPage: widget.onPopPage,
+    );
   }
 
   @override
-  Future<void> setNewRoutePath(TopLevelRoutePath configuration) {
-    throw UnimplementedError();
-  }
+  String? get restorationId => 'top_level_navigator';
 
-  void navigateToTopLevelPage(TopLevelNavigation navigation) {
-    if (navigation == TopLevelNavigation.discover) {
-      _backStack = [const DiscoverRoutePath()];
-    } else {
-      _backStack = [const DiscoverRoutePath(), navigation.toRoutePath()];
-    }
-
-    notifyListeners();
-
-// TODO:
-//     FirebaseAnalytics.instance
-//         .logAniFlowPathChangeEvent(navigation.toRoutePath());
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(topLevel, 'top_level_value');
   }
 }
