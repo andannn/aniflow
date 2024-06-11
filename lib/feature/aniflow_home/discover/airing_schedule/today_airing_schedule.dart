3dff7bffde1ef8f970a53a6eebee6659e1033f7f
andannn <jqn296763005@gmail.com>
2024-06-11 14:27:08 +0900

    ui

--- lib/feature/aniflow_home/discover/airing_schedule/today_airing_schedule.dart
+++ lib/feature/aniflow_home/discover/airing_schedule/today_airing_schedule.dart
@@ -1,3 +1,9 @@
+import 'package:aniflow/core/common/util/string_resource_util.dart';
+import 'package:aniflow/core/data/model/media_model.dart';
+import 'package:aniflow/core/data/model/media_title_model.dart';
+import 'package:aniflow/core/data/user_data_repository.dart';
+import 'package:aniflow/core/design_system/widget/media_preview_item.dart';
+import 'package:aniflow/feature/airing_schedule/schedule_category.dart';
 import 'package:aniflow/feature/airing_schedule/schedule_page_state.dart';
 import 'package:aniflow/feature/aniflow_home/discover/airing_schedule/today_airing_schedule_bloc.dart';
 import 'package:aniflow/feature/aniflow_home/discover/airing_schedule/today_airing_schedule_state.dart';
@@ -25,15 +31,118 @@ class TodayAiringSchedule extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<TodayAiringScheduleBloc, TodayAiringScheduleState>(
       builder: (context, state) {
-
         final categories = state.schedules.toScheduleCategory();
+        if (categories.isEmpty) {
+          return const SizedBox();
+        }
 
-        return Column(
-          children: categories.map((e) => Card(
-            child: Text(e.toString()),
-          )).toList(),
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: IntrinsicHeight(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: categories.map(_buildItem).toList(),
+              ),
+            ),
+          ),
         );
-      }
+      },
+    );
+  }
+
+  Widget _buildItem(ScheduleCategory category) {
+    final isFutureSchedule =
+        TimeOfDay.now().hour <= category.timeOfDayHeader!.hour;
+    return Opacity(
+      opacity: isFutureSchedule ? 1 : 0.4,
+      child: _TimeLineItem(category: category),
+    );
+  }
+}
+
+class _TimeLineItem extends StatelessWidget {
+  const _TimeLineItem({required this.category});
+
+  final ScheduleCategory category;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, bottom: 48),
+          child: Row(
+            children: category.schedules
+                .map((e) => e.animeModel)
+                .map(
+                  (media) => mediaItemBuilder(context, media),
+                )
+                .toList(),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: SizedBox(
+            height: 48,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                        size: 16,
+                        Icons.circle_outlined,
+                        color: Theme.of(context).colorScheme.primary),
+                    Expanded(
+                      child: _buildTimeLineDecoration(
+                        color: colorScheme.tertiary.withAlpha(128),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Text(
+                  context.materialLocal
+                      .formatTimeOfDay(category.timeOfDayHeader!),
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget mediaItemBuilder(BuildContext context, MediaModel media) {
+    final userTitleLanguage =
+        GetIt.instance.get<UserDataRepository>().userData.userTitleLanguage;
+    return SizedBox(
+      width: 160,
+      child: MediaPreviewItem(
+        onClick: () {},
+        coverImage: media.coverImage!.extraLarge!,
+        titleVerticalPadding: 5,
+        title: media.title?.getTitle(userTitleLanguage) ?? '',
+      ),
+    );
+  }
+
+  Widget _buildTimeLineDecoration({required Color color}) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(width: 6),
+        Container(
+          height: 4,
+          color: color,
+        ),
+      ],
     );
   }
 }
