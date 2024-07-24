fcf02f0c18855a0f25599262b78763eb87848c1b
andannn <jqn296763005@gmail.com>
2024-07-24 20:38:34 +0900

    #128

--- lib/core/design_system/icons/icons.dart
+++ lib/core/design_system/icons/icons.dart
@@ -2,4 +2,5 @@
 mixin ATIcons {
   static const String icYoutube = 'assets/image/ic_youtube_xml';
   static const String icNewBadge = 'assets/image/new_badge.png';
+  static const String icApp = 'assets/image/ic_launcher-playstore.png';
 }
