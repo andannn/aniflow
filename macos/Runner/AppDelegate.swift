f84d4b056e892840e1cc52ff8880c164061d3282
andannn <jqn296763005@gmail.com>
2024-12-22 20:37:52 +0900

    event channel in macos

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,11 +3,45 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    static let channelName = "com.andannn.animetracker/auth"
+    private var eventSink: FlutterEventSink?
 
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        guard let controller = mainFlutterWindow?.contentViewController as? FlutterViewController else { return }
+        let eventChannel = FlutterEventChannel(name: AppDelegate.channelName, binaryMessenger: controller.engine.binaryMessenger)
+        eventChannel.setStreamHandler(self)
+
+        super.applicationDidFinishLaunching(notification)
+    }
+
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication)    -> Bool {
+      return true
+    }
+
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+      return true
+    }
+}
+
+
+extension AppDelegate: FlutterStreamHandler {
+    override func application(_ application: NSApplication, open urls: [URL]) {
+        for url in urls {
+            if url.scheme == "animetracker" {
+                eventSink?(url.absoluteString.replacingOccurrences(of: "#", with: "?"))
+                eventSink?(FlutterEndOfEventStream)
+                eventSink = nil
+            }
+        }
+    }
+
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events
+        return nil
+    }
+
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil
+        return nil
+    }
 }
