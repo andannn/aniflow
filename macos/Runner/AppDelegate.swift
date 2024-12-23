f0c61ef68678508fbd96f14fcc4f442fb3d623ef
andannn <jqn296763005@gmail.com>
2024-12-23 09:50:04 +0900

    add CFBundleURLTypes in info.plist

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,26 +3,26 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-    static let channelName = "com.andannn.animetracker/auth"
-    private var eventSink: FlutterEventSink?
+	static let channelName = "com.andannn.animetracker/auth"
+	private var eventSink: FlutterEventSink?
+	
+	override func applicationDidFinishLaunching(_ notification: Notification) {
 
-    override func applicationDidFinishLaunching(_ notification: Notification) {
-        guard let controller = mainFlutterWindow?.contentViewController as? FlutterViewController else { return }
-        let eventChannel = FlutterEventChannel(name: AppDelegate.channelName, binaryMessenger: controller.engine.binaryMessenger)
-        eventChannel.setStreamHandler(self)
+		guard let controller = mainFlutterWindow?.contentViewController as? FlutterViewController else { return }
+		let eventChannel = FlutterEventChannel(name: AppDelegate.channelName, binaryMessenger: controller.engine.binaryMessenger)
+		eventChannel.setStreamHandler(self)
+		
+		super.applicationDidFinishLaunching(notification)
+	}
 
-        super.applicationDidFinishLaunching(notification)
-    }
+	override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication)    -> Bool {
+		return true
+	}
 
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication)    -> Bool {
-      return true
-    }
-
-    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-      return true
-    }
+	override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
 }
-
 
 extension AppDelegate: FlutterStreamHandler {
     override func application(_ application: NSApplication, open urls: [URL]) {
